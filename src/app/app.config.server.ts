3c0b1ccb35400a1d6f6ab6c9542363ecacd245af
Devillance079 <jashwanthlaalu@gmail.com>
2026-01-22 15:37:55 +0530

    c

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -4,7 +4,7 @@ import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering() // <--- THIS is what was missing causing the error
+    provideServerRendering()
   ]
 };
 
